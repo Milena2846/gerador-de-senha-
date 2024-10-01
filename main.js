82c2e6f60e07bf5682e8bf263b2b692fd9ddc764
Milena2846 <129766625+Milena2846@users.noreply.github.com>
2024-10-01 08:37:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,26 +4,47 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-console.log(botoes)
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
+
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
        // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
-    function aumentaTamanho(){
-        if (tamanhoSenha < 20){
+    geraSenha();
+}
+ function aumentaTamanho(){
+    if (tamanhoSenha < 20){
            // tamanhoSenha = tamanhoSenha+1;
            tamanhoSenha++;
-        }
+     }
         numeroSenha.textContent = tamanhoSenha;
+        geraSenha();
     }
-    <input name="senha" type="text" id="campo-senha" readonly>
-        const campoSenha = document.querySelector('#campo-senha');
-        const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelector('.checkbox');
+
+console.log(checkbox[1].checked);
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+geraSenha();
+
+function geraSenha(){
+    let senha ='';
+    if(checkbox[0].checked){
+        alfabeto
+    }
+    for (let i = 0; i <tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.lenght;
+        numeroAleatorio = Math.floor([numeroAleatorio]);
+       senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
 campoSenha.value = letrasMaiusculas;
-    </input>
 }
